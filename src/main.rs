56becd60e4eb3c5ff33a52cc6c7281b51c9c9451
Beetroot Paul <paul@beetrootpaul.com>
2023-02-19 22:32:58 +0100

    Bevy Greeter with Resources

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use bevy::app::App;
-use bevy::prelude::{Commands, Component, Plugin, Query, Res, Time, With};
+use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(bevy::DefaultPlugins)
+        .add_plugins(DefaultPlugins)
         .add_plugin(HelloWorldPlugin)
         .run();
 }
@@ -12,11 +11,25 @@ pub struct HelloWorldPlugin;
 
 impl Plugin for HelloWorldPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(ss_add_people)
-            .add_system(s_hello_world)
-            .add_system(s_greet_people);
+        app.insert_resource(GeneralGreetingTimer(Timer::from_seconds(
+            3.0,
+            TimerMode::Repeating,
+        )))
+        .insert_resource(PersonGreetingTimer(Timer::from_seconds(
+            2.0,
+            TimerMode::Repeating,
+        )))
+        .add_startup_system(ss_add_people)
+        .add_system(s_hello_world)
+        .add_system(s_greet_people);
     }
 }
+
+#[derive(Resource)]
+struct GeneralGreetingTimer(Timer);
+
+#[derive(Resource)]
+struct PersonGreetingTimer(Timer);
 
 #[derive(Component)]
 struct Person;
@@ -30,12 +43,20 @@ fn ss_add_people(mut commands: Commands) {
     commands.spawn((Person, Name("3. Person".to_string())));
 }
 
-fn s_hello_world() {
-    println!("Hello, world!");
+fn s_hello_world(time: Res<Time>, mut timer: ResMut<GeneralGreetingTimer>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        println!("Hello, world!");
+    }
 }
 
-fn s_greet_people(time: Res<Time>, query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("I'm greeting you, {}!", name.0);
+fn s_greet_people(
+    time: Res<Time>,
+    mut timer: ResMut<PersonGreetingTimer>,
+    query: Query<&Name, With<Person>>,
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("I'm greeting you, {}!", name.0);
+        }
     }
 }
