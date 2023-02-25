6e12425f8dc6f39ffdeaa571c7eed04bf52a756c
Beetroot Paul <paul@beetrootpaul.com>
2023-02-25 22:53:20 +0100

    draw debug lines around sprites + exclude debug stuff from a release build

--- release.sh
+++ release.sh
@@ -18,3 +18,9 @@ cargo build --release
 # TODO: web: panic messages https://bevy-cheatbook.github.io/platforms/wasm/panic-console.html
 # TODO: web: optimize for size https://bevy-cheatbook.github.io/platforms/wasm/size-opt.html
 # TODO: web: hosting https://bevy-cheatbook.github.io/platforms/wasm/gh-pages.html
+
+rm -rf ./target/release/assets/
+mkdir -p ./target/release/assets/
+cp ./assets/* ./target/release/assets/
+
+./target/release/avoid_your_past_rust_bevy
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::ops::Add;
 use std::time::Duration;
 
-use bevy::math::vec2;
+use bevy::math::{vec2, vec3, Vec3Swizzles};
 use bevy::prelude::*;
 use bevy::sprite::Anchor;
 use iyes_loopless::prelude::AppLooplessFixedTimestepExt;
@@ -78,42 +79,51 @@ const VIEWPORT_H: f32 = TOPBAR_H + GAME_AREA_H;
 
 // TODO game states https://bevy-cheatbook.github.io/programming/states.html
 
-const FIXED_FPS: u64 = 60;
+const FIXED_FPS: u64 = 30;
 
 const FIXED_TIMESTEP_GAME_LOOP: &str = "fixed_timestep_game_loop";
 
+const SPRITE_SHEET_SPRITE_W: f32 = 8.;
+const SPRITE_SHEET_SPRITE_H: f32 = 8.;
+
 fn main() {
-    App::new()
-        .add_plugins(
-            DefaultPlugins
-                .set(WindowPlugin {
-                    window: WindowDescriptor {
-                        title: GAME_TITLE.to_string(),
-                        // width: SCALE * VIEWPORT_W,
-                        // height: SCALE * VIEWPORT_H,
-                        ..default()
-                    },
+    let mut app = App::new();
+    app.add_plugins(
+        DefaultPlugins
+            .set(WindowPlugin {
+                window: WindowDescriptor {
+                    title: GAME_TITLE.to_string(),
+                    // width: SCALE * VIEWPORT_W,
+                    // height: SCALE * VIEWPORT_H,
                     ..default()
-                })
-                // Prevent blurring of scaled up pixel art sprites
-                .set(ImagePlugin::default_nearest())
-                .set(AssetPlugin {
-                    // Watch for changes in assets and hot-reload them without need to run the app again
-                    watch_for_changes: true,
-                    ..default()
-                }),
-        )
-        // TODO: group both plugins below into a single plugin for pixel upscaling camera
-        .add_plugin(bevy_pixel_camera::PixelCameraPlugin)
-        .add_plugin(bevy_pixel_camera::PixelBorderPlugin {
-            color: bevy_color_from(Pico8Color::Black),
-        })
-        // TODO merge both FPS-related diagnostics lines into a single well-named plugin
-        // Print FPS in a console
-        .add_plugin(bevy::diagnostic::LogDiagnosticsPlugin::default())
-        .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin::default())
-        // Get rid of edges of neighbour sprites visible around the given sprite from the sprite sheet
-        .insert_resource(Msaa { samples: 1 })
+                },
+                ..default()
+            })
+            // Prevent blurring of scaled up pixel art sprites
+            .set(ImagePlugin::default_nearest())
+            .set(AssetPlugin {
+                // Watch for changes in assets and hot-reload them without need to run the app again
+                watch_for_changes: true,
+                ..default()
+            }),
+    )
+    // TODO: group both plugins below into a single plugin for pixel upscaling camera
+    .add_plugin(bevy_pixel_camera::PixelCameraPlugin)
+    .add_plugin(bevy_pixel_camera::PixelBorderPlugin {
+        color: bevy_color_from(Pico8Color::Black),
+    });
+
+    // TODO merge both FPS-related diagnostics lines into a single well-named plugin
+    // Print FPS in a console
+    #[cfg(debug_assertions)]
+    app.add_plugin(bevy::diagnostic::LogDiagnosticsPlugin::default())
+        .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin::default());
+
+    #[cfg(debug_assertions)]
+    app.add_plugin(bevy_prototype_debug_lines::DebugLinesPlugin::default());
+
+    // Get rid of edges of neighbour sprites visible around the given sprite from the sprite sheet
+    app.insert_resource(Msaa { samples: 1 })
         // Draw a solid background color
         .insert_resource(ClearColor(bevy_color_from(Pico8Color::Black)))
         .add_startup_system(spawn_camera)
@@ -121,15 +131,23 @@ fn main() {
         .add_startup_system(spawn_player)
         // TODO: will it affect HTML embedded game?
         .add_system(bevy::window::close_on_esc)
-        .add_system(handle_keyboard_input)
-        .add_fixed_timestep(
-            Duration::from_nanos(1_000_000_000 / FIXED_FPS),
-            FIXED_TIMESTEP_GAME_LOOP,
-        )
-        .add_fixed_timestep_system(FIXED_TIMESTEP_GAME_LOOP, 0, debug_fixed)
-        .add_fixed_timestep_system(FIXED_TIMESTEP_GAME_LOOP, 0, fixed_update_player)
-        .add_fixed_timestep_system(FIXED_TIMESTEP_GAME_LOOP, 0, fixed_update_player_sprite)
-        .run();
+        .add_system(handle_keyboard_input);
+
+    #[cfg(debug_assertions)]
+    app.add_system(draw_debug_sprite_boundaries);
+
+    app.add_fixed_timestep(
+        Duration::from_nanos(1_000_000_000 / FIXED_FPS),
+        FIXED_TIMESTEP_GAME_LOOP,
+    );
+
+    #[cfg(debug_assertions)]
+    app.add_fixed_timestep_system(FIXED_TIMESTEP_GAME_LOOP, 0, debug_fixed);
+
+    app.add_fixed_timestep_system(FIXED_TIMESTEP_GAME_LOOP, 0, fixed_update_player)
+    .add_fixed_timestep_system(FIXED_TIMESTEP_GAME_LOOP, 0, fixed_update_player_sprite);
+
+    app.run();
 }
 
 // Copied from https://github.com/IyesGames/iyes_loopless#fixed-timestep-control
@@ -185,7 +203,7 @@ fn spawn_game_area(mut commands: Commands) {
 }
 
 // TODO: note it down somewhere or make sure it is satisfied in the code itself
-//       (taken from bevy_pixel_camera plugin's README):
+//       (taken from bevy_pixel_camera plugin's README https://github.com/drakmaniso/bevy_pixel_camera#bevy_pixel_camera ):
 //       Note that if either the width or the height of your sprite is not divisible by 2,
 //       you need to change the anchor of the sprite (which is at the center by default),
 //       or it will not be pixel aligned.
@@ -197,8 +215,14 @@ fn spawn_player(
 ) {
     // TODO: move atlas creation out of the system. Atlas will be used by many entities, possibly created in their separate setup systems
     let sprite_sheet_handle: Handle<Image> = asset_server.load("spritesheet.png");
-    let texture_atlas =
-        TextureAtlas::from_grid(sprite_sheet_handle, vec2(8., 8.), 16, 3, None, None);
+    let texture_atlas = TextureAtlas::from_grid(
+        sprite_sheet_handle,
+        vec2(SPRITE_SHEET_SPRITE_W, SPRITE_SHEET_SPRITE_H),
+        16,
+        3,
+        None,
+        None,
+    );
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
     // TODO: animated sprite https://github.com/bevyengine/bevy/blob/latest/examples/2d/sprite_sheet.rs
@@ -265,11 +289,72 @@ fn fixed_update_player_sprite(mut query: Query<(&ControlledDirection, &mut Textu
     }
 }
 
+fn draw_debug_sprite_boundaries(
+    mut lines: ResMut<bevy_prototype_debug_lines::DebugLines>,
+    query: Query<(&Transform, &TextureAtlasSprite)>,
+) {
+    for (transform, _sprite) in query.iter() {
+        // TODO: can we do it in a shorter way?
+        lines.line(
+            transform.translation.xyz().add(vec3(
+                SPRITE_SHEET_SPRITE_W / 2.,
+                SPRITE_SHEET_SPRITE_H / 2.,
+                0.,
+            )),
+            transform.translation.xyz().add(vec3(
+                -SPRITE_SHEET_SPRITE_W / 2.,
+                SPRITE_SHEET_SPRITE_H / 2.,
+                0.,
+            )),
+            0.,
+        );
+        lines.line(
+            transform.translation.xyz().add(vec3(
+                -SPRITE_SHEET_SPRITE_W / 2.,
+                SPRITE_SHEET_SPRITE_H / 2.,
+                0.,
+            )),
+            transform.translation.xyz().add(vec3(
+                -SPRITE_SHEET_SPRITE_W / 2.,
+                -SPRITE_SHEET_SPRITE_H / 2.,
+                0.,
+            )),
+            0.,
+        );
+        lines.line(
+            transform.translation.xyz().add(vec3(
+                -SPRITE_SHEET_SPRITE_W / 2.,
+                -SPRITE_SHEET_SPRITE_H / 2.,
+                0.,
+            )),
+            transform.translation.xyz().add(vec3(
+                SPRITE_SHEET_SPRITE_W / 2.,
+                -SPRITE_SHEET_SPRITE_H / 2.,
+                0.,
+            )),
+            0.,
+        );
+        lines.line(
+            transform.translation.xyz().add(vec3(
+                SPRITE_SHEET_SPRITE_W / 2.,
+                -SPRITE_SHEET_SPRITE_H / 2.,
+                0.,
+            )),
+            transform.translation.xyz().add(vec3(
+                SPRITE_SHEET_SPRITE_W / 2.,
+                SPRITE_SHEET_SPRITE_H / 2.,
+                0.,
+            )),
+            0.,
+        );
+    }
+}
+
 fn fixed_update_player(
     mut query: Query<(&ControlledDirection, &mut Transform, Option<&SpritePadding>)>,
 ) {
     // TODO: is it possible to bind speed to FPS (change in FPS -> automatic change of speed to make it constant in result), without allowing for non-integers?
-    const MOVEMENT_PER_FRAME: f32 = 1.;
+    const MOVEMENT_PER_FRAME: f32 = 2.;
 
     for (controlled_direction, mut transform, maybe_sprite_padding) in query.iter_mut() {
         match controlled_direction {
