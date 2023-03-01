475637c294cc6959cc053ae90f1faaa19f4d5343
Beetroot Paul <paul@beetrootpaul.com>
2023-03-01 13:11:44 +0100

    [GitHub Actions] add missing WASM target and split a job into multiple smaller ones

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ rustup update stable
 rustup default stable
 
 # Based on https://bevy-cheatbook.github.io/platforms/wasm.html
-rustup target install wasm32-unknown-unknown
+rustup target add wasm32-unknown-unknown
 cargo install wasm-server-runner
