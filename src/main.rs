e1a376a529fd1039033547ae72ea302b71161c10
Beetroot Paul <paul@beetrootpaul.com>
2023-03-12 02:09:40 +0100

    wip5

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,4 @@ fn main() {
 //      - res 1 : https://bevy-cheatbook.github.io/programming/system-order.html
 //      - res 2 : https://bevy-cheatbook.github.io/programming/system-sets.html
 
-// TODO: tests 1 https://github.com/bevyengine/bevy/blob/latest/tests/how_to_test_systems.rs
-// TODO: tests 2 https://bevy-cheatbook.github.io/programming/system-tests.html
-
 // TODO: pixel art rendering discord://discord.com/channels/691052431525675048/1075359868631928933/1075359868631928933
